b63268a072760b3808f81c0d929a353f819d3b6a
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-07-02 13:58:51 +0530

    Links

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -1,7 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
 
+const navLinks = [
+    { name: "Link1", href: "/"},
+    {name: "Link2" , href: "/Products/1"}
+]
 export default function About() {
     return (
-        <h1>About</h1>
+        <>
+            <h1>About</h1>
+            {navLinks.map((x) => {
+                return (
+                    <Link href={x.href} key={x.name}>
+                        {x.name}
+                    </Link>
+                )
+            })}
+        </>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
+    <>
     <h1>HELLO</h1>
+      <Link href="/About">About</Link>
+    </>
   );
 }
