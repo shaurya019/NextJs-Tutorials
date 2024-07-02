a0f5b41a3101049cd522e946e50bd358e1251c5d
shaurya019 <82964271+shaurya019@users.noreply.github.com>
2024-07-02 11:09:50 +0530

    NOT FOUND PAGE

--- src/app/Products/[id]/page.tsx
+++ src/app/Products/[id]/page.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
+import { notFound } from "next/navigation";
 
 export default function PageId({ params }: any) {
+    if (parseInt(params.id) > 100) {
+        notFound();
+    }
     return (
         <h1>Page - {params.id}</h1>
     );
---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const notFound = () => {
+    return (
+        <div>
+            <h1>NOT FOUND</h1>
+        </div>
+    )
+}
+
+export default notFound
